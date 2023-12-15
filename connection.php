4686b10ad86882eb60763045faf6d4e42522d1d5
Hirun Chamara <astronomyhirunchamara@gmail.com>
2023-12-15 22:42:12 +0530

    adjusted .env variables

--- connection.php
+++ connection.php
@@ -13,7 +13,9 @@ class Database{
             $db_host = $_ENV["DB_HOST"];
             $db_user = $_ENV["DB_USER"];
             $db_pass = $_ENV["DB_PASS"];
-            Database::$connection = new mysqli($db_host, $db_user, $db_pass,"eshop","3306");
+            $db_name = $_ENV["DB_NAME"];
+            $db_port = $_ENV["DB_PORT"];
+            Database::$connection = new mysqli($db_host, $db_user, $db_pass,$db_name,$db_port);
         }
     }
 
