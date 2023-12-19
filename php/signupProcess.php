14a9e77ea05e9116ce904ecd26baa818d3b5a93d
Hirun Chamara <astronomyhirunchamara@gmail.com>
2023-12-19 22:19:54 +0530

    fixed issues with signup php function

--- php/signupProcess.php
+++ php/signupProcess.php
@@ -10,30 +10,29 @@ $gender = $_POST["g"];
 
 if(empty($fname)){
     echo("Please Enter Your First Name.");
-}else if(strlen($fname) > 50){
+}elseif(strlen($fname) > 50){
     echo("First Name Must Contain LOWER THAN 50 Characters.");
-}else if(empty($lname)){
+}elseif(empty($lname)){
     echo("Please Enter Your Last Name.");
-}else if(strlen($lname) > 50){
+}elseif(strlen($lname) > 50){
     echo("Last Name Must Contain LOWER THAN 50 Characters.");
-}else if(empty($email)){
+}elseif(empty($email)){
     echo("Please Enter Your Email Address.");
-}else if(strlen($email) > 100){
+}elseif(strlen($email) > 100){
     echo("Email Address Must Contain LOWER THAN 100 Characters.");
-}else if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+}elseif(!filter_var($email,FILTER_VALIDATE_EMAIL)){
     echo("Invalid Email Address.");
-}else if(empty($password)){
+}elseif(empty($password)){
     echo("Please Enter Your Password.");
-}else if(strlen($password) < 5 || strlen($password) > 20){
+}elseif(strlen($password) < 5 || strlen($password) > 20){
     echo("Password Must Contain 5 to 20 Characters.");
-}else if(empty($mobile)){
+}elseif(empty($mobile)){
     echo("Please Enter Your Mobile Number.");
 }else if(strlen($mobile) != 10){
     echo("Mobile Number Must Contain 10 Characters.");
-}else if(!preg_match("/07[0,1,2,4,5,6,7,8][0-9]/", $mobile)){
+}elseif(!preg_match("/07[0,1,2,4,5,6,7,8][0-9]/", $mobile)){
     echo("Invalid Mobile Number.");
-}
-
+} else {
     $rs = Database::search("SELECT * FROM `users` WHERE `email`='".$email."' OR `mobile`='".$mobile."'");
     $n = $rs->num_rows;
 
@@ -51,5 +50,8 @@ if(empty($fname)){
 
         echo ("success");
     }
+}
+
+    
 
 ?>
